2ec137e067a641fdb176aba08028689b37e799b3
Willtrigo <willtrigo@gmail.com>
2018-10-31 21:30:54 -0300

    Update settings

--- pqb/settings.py
+++ pqb/settings.py
@@ -83,4 +83,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
