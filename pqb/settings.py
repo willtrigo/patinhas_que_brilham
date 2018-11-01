2c48274f881f2ffccab61db8c5406df8c5b8fbe4
Willtrigo <willtrigo@gmail.com>
2018-10-31 21:11:24 -0300

    Update settings

--- pqb/settings.py
+++ pqb/settings.py
@@ -82,3 +82,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
